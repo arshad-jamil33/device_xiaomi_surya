53fc731056c83df0d8ac157bcfdbb23df20fee55
NothingHere69 <numanaftab5432@gmail.com>
2026-01-26 19:09:27 +0000

    surya: Match vendor security patch with android security patch
    Signed-off-by: NothingHere69 <numanaftab5432@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,7 +171,7 @@ SOONG_CONFIG_xiaomiSuryaVars_livedisplay_support_anti_flicker ?= false
 SOONG_CONFIG_xiaomiSuryaVars_livedisplay_support_sunlight_enhancement ?= true
 
 # Vendor security patch level
-VENDOR_SECURITY_PATCH := 2026-01-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
